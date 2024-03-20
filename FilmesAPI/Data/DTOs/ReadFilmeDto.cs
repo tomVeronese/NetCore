bc8bdbf0ae4e930b3d0ad16038d8efbf5c482456
tomVeronese <tom_veronese@hotmail.com>
2024-03-20 16:25:10 -0300

    Add files via upload

---
+++ FilmesAPI/Data/DTOs/ReadFilmeDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.DTOs;
+
+public class ReadFilmeDto
+{
+    public string Titulo { get; set; }
+
+    public string Genero { get; set; }
+
+    public int Duracao { get; set; }
+
+    public string Diretor { get; set; }
+
+    public DateTime HoraDaConsulta { get; set; } = DateTime.Now;
+}
